18a74ca07b18aee4d9a971b330202f661978b81a
elenawinters <elenamaywinters@gmail.com>
2022-04-21 20:01:59 -0500

    Version Bump - Change License to MIT
    I should've picked this one originally. It fits this project a lot better, as I don't really care what you do with a closed source version of this.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ games { 'gta5' }
 
 author 'Elena Winters'
 description 'Shows amount of damage you dealt to an entity next to that entity'
-version '0.1.0'
+version '0.1.1'
 
 client_script 'dmgclient.lua'
 client_script 'dmghud.lua'
