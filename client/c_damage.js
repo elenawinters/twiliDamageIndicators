e6af6f358d2ae36488d56e072c50fb7034ea3669
Elena Winters <elenamaywinters@gmail.com>
2023-05-29 02:34:02 -0500

    Make RedM compatible again

--- client/c_damage.js
+++ client/c_damage.js
@@ -168,9 +168,9 @@ function DrawDamageText(position, value, color, size = 1, rate, entity) {
                 // EndTextCommandDisplayText(_x, _y);
                 // console.log('Did a render');
             } else if (exports.twiliCore.GAME() == exports.twiliCore.REDM()) {
-                let vstr = CreateVarString(10, "LITERAL_STRING", tostring(value));  // the game will crash if VALUE is not a string
+                let vstr = CreateVarString(10, "LITERAL_STRING", value.toString());  // the game will crash if VALUE is not a string
                 SetTextColor(color[0], color[1], color[2], currentAlpha);
-                SetTextScale(parseInt(scaleMultiplier * 0.0), parseInt(0.35 * scaleMultiplier));
+                SetTextScale(scaleMultiplier * 0.0, 0.35 * scaleMultiplier);
                 SetTextFontForCurrentCommand(font);
                 SetTextCentre(true);
 
@@ -246,8 +246,8 @@ if (exports.twiliCore.GAME() == exports.twiliCore.FIVEM()) {
 
 
 onNet('twiliCore:damage:event', (suspect, victim, situation) => {
-    if (suspect != PlayerPedId() && victim != PlayerPedId() && Settings.local_damage) { return; }
-    const fadeRate = CalculateFadeRate(situation.isMelee, situation.weaponHash);
+    if (suspect.entity != PlayerPedId() && victim.entity != PlayerPedId() && Settings.local_damage) { return; }
+    const fadeRate = exports.twiliCore.GAME() == exports.twiliCore.FIVEM() ? CalculateFadeRate(situation.isMelee, situation.weaponHash) : 1;
     let skip_damage_render = false
     if (Settings.ignore_vehicles) {
         if (IsEntityAVehicle(victim.entity)) {
@@ -262,16 +262,17 @@ onNet('twiliCore:damage:event', (suspect, victim, situation) => {
         SendNUIMessage({play_audio: situation.isDead ? killSound : hitSound});
     }
 
+    // This is pretty janky code.
     // let dmg = CalculateHealthLost(victim.entity)
     if (!skip_damage_render) {
         // if (IsEntityAPed(victim) && IsPedFatallyInjured(victim) && dmg.h != 0) {  // consider using victimDied instead of IsPedFatallyInjured
-        if (IsEntityAPed(victim.entity) && situation.isDead && situation.healthLost.h != 0) {
+        if (exports.twiliCore.GAME() == exports.twiliCore.FIVEM() && IsEntityAPed(victim.entity) && situation.isDead && situation.healthLost.h != 0) {
             DrawDamageText(situation.position, exports.twiliCore.math().round(-situation.healthLost.h + 100, Settings.precision), Settings.color.damage_entity, 1, fadeRate, victim.entity)
         } else {
             DrawDamageText(situation.position, exports.twiliCore.math().round(-situation.healthLost.h, Settings.precision), Settings.color.damage_entity, 1, fadeRate, victim.entity)
         }
         
-        if (situation.healthLost.a != 0) {
+        if (situation.healthLost.a != 0 && situation.healthLost.a != null) {
             DrawDamageText(situation.position, exports.twiliCore.math().round(-situation.healthLost.a, Settings.precision), Settings.color.damage_armor, 1, fadeRate, victim.entity)
         }
         
@@ -304,190 +305,3 @@ onNet('twiliCore:damage:event', (suspect, victim, situation) => {
     // PrepareDamageText(victim.entity, situation.isDead, situation.healthLost, situation.position, fadeRate)
 
 });
-// onNet('twiliCore:damage:event')
-
-    // on('CEventDamage', function (victims, suspect) {
-    //     // console.log(victims);
-    //     // console.log(suspect);
-    //     for (let [, victim] of Object.entries(victims)) {
-    //         if (!IsPedAPlayer(suspect) || !IsPedAPlayer(victim)) { return; }
-    //         const dmg = CalculateHealthLost(victim);
-    //         const position = CalculateDamagePosition(suspect, victim);
-    //         const weaponHash = GetPedCauseOfDeath(victim);
-    //         const isMelee = GetWeaponDamageType(weaponHash) == 2;
-    //         const fadeRate = CalculateFadeRate(isMelee, weaponHash);
-
-    //         emitNet("ewdamagenumbers:update_others", suspect, victim, dmg, position, fadeRate);
-    //     }
-    //     // if (!skip_damage_render) {
-    //     //     if (IsEntityAPed(victim) && IsPedFatallyInjured(victim) && dmg.h != 0) {
-    //     //         DrawDamageText(position, Math.round(-dmg.h + 100), Settings['color']['damage_entity'], 1, fadeRate, victim)
-    //     //     } else {
-    //     //         DrawDamageText(position, Math.round(-dmg.h), Settings['color']['damage_entity'], 1, fadeRate, victim)
-    //     //     }
-            
-    //     //     if (dmg.a != 0) {
-    //     //         DrawDamageText(position, Math.round(-dmg.a), Settings['color']['damage_armor'], 1, fadeRate, victim)
-    //     //     }
-    //     // }
-
-    // })
-
-    // use GetWeaponTimeBetweenShots to get dynamic fade speed per weapon
-    // use log 0.7 (x) to get Fade Speed from TimeBetweenShots. If output is below 0.1, keep it at 0.1. Refine later
-    // on('gameEventTriggered', function (eventName, data) {
-    //     if (eventName != 'CEventNetworkEntityDamage') { return; }
-    //     // console.log(Settings.color.damage_entity);
-
-    //     const victim = data[0];
-    //     // console.log(typeof victim);
-    //     const suspect = data[1];
-
-    //     if (IsPedAPlayer(suspect) && IsPedAPlayer(victim)) { return; }
-
-    //     if (suspect != PlayerPedId() && victim != PlayerPedId() && Settings.local_damage) { return; }
-    //     // if (DebugFlags.prefer_pvp == true && !IsEntityAVehicle(victim)) { return; }
-    //     // console.log('This should only show if it is a vehicle');
-
-    //     let offset = 0;
-
-    //     if (BUILD >= 2060) {
-    //         offset++;
-    //         if (BUILD >= 2189) {
-    //             offset++;
-    //         }
-    //     }
-
-    //     const victimDied = data[3 + offset];
-    //     const weaponHash = data[4 + offset];
-    //     const isMelee = data[9 + offset];
-    //     const damageType = data[10 + offset];
-
-    //     // Would it be funny to render blood drop emotes twice a second for ten seconds when the below is true?
-    //     // Yes.
-    //     // Am I gonna do it...?
-    //     // Maybe, when I clean up everything. It'll be an easter egg that can be toggled
-    //     // console.log(`victimDied? ${victimDied}`)
-
-    //     const position = CalculateDamagePosition(suspect, victim, victimDied);
-
-    //     const fadeRate = CalculateFadeRate(isMelee, weaponHash);
-
-//         let skip_damage_render = false
-//         if (Settings.ignore_vehicles) {
-//             if (IsEntityAVehicle(victim)) {
-//                 skip_damage_render = true
-//             }
-//         }
-
-//         let dmg = CalculateHealthLost(victim)
-//         if (!skip_damage_render) {
-//             // if (IsEntityAPed(victim) && IsPedFatallyInjured(victim) && dmg.h != 0) {  // consider using victimDied instead of IsPedFatallyInjured
-
-//             PrepareDamageText(victim, victimDied, dmg, position, fadeRate)
-//             // if (IsEntityAPed(victim) && victimDied && dmg.h != 0) {  // consider using victimDied instead of IsPedFatallyInjured
-//             //     DrawDamageText(position, Math.round(-dmg.h + 100), Settings.color.damage_entity, 1, fadeRate, victim)
-//             // } else {
-//             //     DrawDamageText(position, Math.round(-dmg.h), Settings.color.damage_entity, 1, fadeRate, victim)
-//             // }
-            
-//             // if (dmg.a != 0) {
-//             //     DrawDamageText(position, Math.round(-dmg.a), Settings.color.damage_armor, 1, fadeRate, victim)
-//             // }
-
-//             // if (victimDied) {
-//             //     SendNUIMessage({play_audio: 'https://wiki.teamfortress.com/w/images/1/14/Killsound.wav'});
-//             // } else {
-//             //     SendNUIMessage({play_audio: 'https://wiki.teamfortress.com/w/images/c/cf/Hitsound.wav'});
-//             // }
-//         }
-
-
-//         switch (damageType) {
-//             case 93:
-//                 DrawDamageText(position, 'Pop!', Settings.color.damage_vehicle_ding, 0.5, fadeRate, victim) // tire damage
-//                 break;
-//             case 116:
-//             case 117:  // 117 happens with the exhaust pipe of the Imponte Pheonix. No idea what this is, putting it here for now.
-//                 DrawDamageText(position, 'Ding!', Settings.color.damage_vehicle_ding, 0.5, fadeRate, victim)  // general vehicle damage
-//                 break;
-//             case 120:
-//             case 121:
-//             case 122:
-//                 DrawDamageText(position, 'Smash!', Settings.color.damage_vehicle_ding, 0.5, fadeRate, victim)  // window damage
-//                 break;
-//         }
-//     })
-// }
-// } else if (GAME == REDM) {
-//     function RenderStepRDR3(victim, suspect, defaultPosition, value) {
-//         let positon, entity = RaycastFromPlayer();
-//     }
-// }
-
-//     end)
-// } else if (GAME == REDM) {  // RedM doesn't have the `gameEventTriggered` handler
-//     function RenderStepRDR3(victim, suspect, defaultPosition, value)  // there is a return, and if you return in the above Thread, it exits the thread
-//         if suspect ~= PlayerPedId() and victim ~= PlayerPedId() and Settings['local_damage'] == true then
-//             return  // if i'm not a victim or suspect, and I only want to show my damage, return
-//         end
-
-//         // TODO: OneSync causes issues relating to the activation of the Game Event. We need to fix it.
-//         // Basically, if a horse isn't visible but dead, it'll despawn, but when it gets back into view, it'll spawn and die, triggering the event.
-//         // Oddity: It only seems to happen with horses that were attached to a carriage.
-
-//         // Potential Solution: Take advantage of TrackedEntities, with a simple data value of alive = true or false
-
-//         // Interesting to note: In Armadillo, the body pits corpses are spawned in as peds and then killed.
-//         // Workaround: Check for those specific models and don't render if they are the entity.
-//         // This could also be a OneSync thing, but I have yet to test that.
-
-//         local position, entity = RaycastFromPlayer()
-
-//         if entity ~= victim then
-//             local is_ped, bone = GetPedLastDamageBone(victim)
-//             if is_ped == 1 then
-//                 position = GetPedBoneCoords(victim, bone)
-//             else
-//                 position = defaultPosition
-//             end
-//         end
-
-//         DrawDamageText(position, round(-value), Settings['color']['damage_entity'], 1, victim)
-
-//     end
-//     Citizen.CreateThread(function()
-//         while true do
-//             Citizen.Wait(0)
-
-//             local size = GetNumberOfEvents(0)
-//             if size > 0 then
-//                 for i = 0, size - 1 do
-//                     local eventAtIndex = GetEventAtIndex(0, i)
-//                     if eventAtIndex == 402722103 then  // EVENT_ENTITY_DAMAGED
-//                     // if eventAtIndex == -1315570756 then  // EVENT_NETWORK_DAMAGE_ENTITY
-//                         local eventDataSize = 9
-//                         local eventDataStruct = DataView.ArrayBuffer(8*eventDataSize)  // not a fan of using DataView here. i'll rewrite it someday
-//                         for iter = 0, eventDataSize - 1 do
-//                             eventDataStruct:SetInt32(8*iter, 0)
-//                         end
-                        
-//                         // local eventDataExists = GetEventData(0, i, eventDataStruct:Buffer(), eventDataSize)  // doesn't work?
-
-//                         local eventDataExists = Citizen.InvokeNative(0x57EC5FA4D4D6AFCA, 0, i, eventDataStruct:Buffer(), eventDataSize)  //GetEventData
-//                         if eventDataExists then
-//                             local victim = eventDataStruct:GetInt32(0)
-//                             local suspect = eventDataStruct:GetInt32(8)
-//                             local position = vector3(eventDataStruct:GetFloat32(8*6), eventDataStruct:GetFloat32(8*7), eventDataStruct:GetFloat32(8*8))
-//                             local value = eventDataStruct:GetFloat32(32)
-
-//                             RenderStepRDR3(victim, suspect, position, value)
-//                         end
-
-//                     end
-//                 end
-//             end
-//         end
-//     end)
-
-// }
